e97237ab65d9474ae9814b9571051cd8a52868cd
Jarrod Flesch <jarrodmflesch@gmail.com>
2019-02-11 16:47:12 -0500

    change image paths in json files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff19ca397c0a33aa4d5f";
+/******/ 	var hotCurrentHash = "25ba1474cf05b9c8fdba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
